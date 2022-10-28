31cfb2b6aecf7c23e9813e9e76181ea1168717a6
lkimdaryl <78583414+lkimdaryl@users.noreply.github.com>
2022-10-28 16:59:55 -0700

    tests

---
+++ LinkedListTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	@Test 
+	public void append() {
+		
+	}
+	
+	@Test 
+	public void firstElement() {
+		
+	}
+	
+	@Test 
+	public void lastElement() {
+		
+	}
+	
+	@Test 
+	public void ToString() {
+		
+	}
+	
+	@Test 
+	public void listLength() {
+		
+	}
+	
+	
+	
+	
+	
+	
+  
+  
+  
+}
